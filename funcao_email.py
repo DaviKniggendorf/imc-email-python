6c7b0ced67087523c0ea4a9b734ae4aad24c17cb
Davi Alexandre <estudoprogram010101@gmail.com>
2021-06-10 14:16:40 -0300

    Desenvolvimento do sistema de email automatico

---
+++ funcao_email.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def enviar_email(destiono):
+	# Configuração
+	host = 'smtp.gmail.com'
+	port = 587
+	user = 'devstudying001@gmail.com'
+	password = open('dados/senha.txt', encoding='UTF-8').read().strip()
+
+	# Criando objeto
+	print('Criando objeto servidor...')
+	server = smtplib.SMTP(host, port)
+
+	# Login com servidor
+	print('Login...')
+	server.ehlo()
+	server.starttls()
+	server.login(user, password)
+
+	# Criando mensagem
+	message = 'Olá, mundo!'
+	print('Criando mensagem...')
+	email_msg = MIMEMultipart()
+	email_msg['From'] = user
+	email_msg['To'] = destiono
+	email_msg['Subject'] = 'Assunto da mensagem'
+	print('Adicionando texto...')
+	email_msg.attach(MIMEText(message, 'plain'))
+
+	# Enviando mensagem
+	print('Enviando mensagem...')
+	server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+	print('Mensagem enviada!')
+	server.quit()
