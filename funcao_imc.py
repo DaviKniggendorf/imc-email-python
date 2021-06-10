7766847b85505b88f60fc98ec7cbd172f0ff1558
Davi Alexandre <estudoprogram010101@gmail.com>
2021-06-10 14:00:51 -0300

    Criando as files

---
+++ funcao_imc.py
@@ -0,0 +1,10 @@
+'''
+Parte responsavel pelas funções de calcular IMC e
+Classificalos de acordo com o resultado do IMC.
+'''
+
+def calcula-imc(altura, peso):
+	imc_calculado = peso / (altura*altura)
+	return imc_calculado
+
+
